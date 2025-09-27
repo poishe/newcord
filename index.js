bd06c21132fcf9b3ce7d95c7ff944ef867d9a65b
poishe <200611sep@gmail.com>
2025-09-27 20:54:18 +0300

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var crypto = require("crypto-js");
 var tmi = require("tmi.js");
 
 app.get("/", function(request, response) {
-response.sendFile("/index.html");
+response.sendFile("index.html");
 });
 
 app.listen(3000);
